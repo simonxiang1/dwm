b7f78d16a7a749ce8aaeb9257d28aae504368b44
Simon Xiang <simonxiang@zohomail.com>
2020-06-18 22:07:06 -0500

    patched in gaps, now uses mononoki

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 ## personal build of dwm
 todo list:
-- patch in gaps
 - patch in a clean bar, includes
 	- time (no more timedatectl)
+	- tags are now the 5 kanji of the monado (斬, 疾, 翔, 盾, 撃)
 	- battery level (no more guessing)
 	- weather? optional
-	- now playing
+	- now playing (nah if too much work)
+	- transparency on bar (alpha patch)
+	- not stuck to the top (bar padding patch)
 - add xresources compatibility (for pywal)
-- transparency for windows (???) + blur (idek how to do this)
-- new layouts
+- new layouts (grid, attachaside, pertag, centered master)
 - custom keybinds for 
 	- brightness (xbacklight -inc/dec 5)
 	- volume (alsamixer?)
+	- ncmpcpp (pause, play, next song, shuffle?)
+
+done:
+- now uses mononoki nerd font
+- gaps patched in, custom binds:
+  - alt-super-j to enlarge gaps (shrink windows)
+  - alt-super-k to decrease gaps (enlarge windows)
+  - alt-super-0 to reset gaps and alt-super-shift-0 to toggle gaps
 
--- config.def.h
+++ config.def.h
@@ -3,10 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "mononoki Nerd Font:size=11" };
+static const char dmenufont[]       = "mononoki Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -34,7 +39,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -70,6 +75,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|Mod4Mask,              XK_j,      incrgaps,       {.i = +2 } },
+	{ MODKEY|Mod4Mask,              XK_k,      incrgaps,       {.i = -2 } },
+	{ MODKEY|Mod4Mask,              XK_0,	   defaultgaps,    {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      togglegaps,     {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
