ccbbcf2caffe5d15988e322e56ea88ab0c664b80
Simon Xiang <simonxiang@zohomail.com>
2020-06-18 23:26:50 -0500

    added autostart (pywal still not starting)

--- config.h
+++ config.h
@@ -26,6 +26,11 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"st", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "斬", "疾", "翔", "盾", "撃"};
 
