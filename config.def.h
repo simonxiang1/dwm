8d34bf19919918e07cf72407c50e4baa4c1043b5
Simon Xiang <simonxiang@zohomail.com>
2020-06-18 22:31:59 -0500

    changed default gap size, monado arts activated

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "斬", "疾", "翔", "盾", "撃"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -75,8 +75,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_j,      incrgaps,       {.i = +2 } },
-	{ MODKEY|Mod4Mask,              XK_k,      incrgaps,       {.i = -2 } },
+	{ MODKEY|Mod4Mask,              XK_j,      incrgaps,       {.i = +3 } },
+	{ MODKEY|Mod4Mask,              XK_k,      incrgaps,       {.i = -3 } },
 	{ MODKEY|Mod4Mask,              XK_0,	   defaultgaps,    {0} },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      togglegaps,     {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
@@ -98,10 +98,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "斬", "疾", "翔", "盾", "撃"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -75,8 +75,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_j,      incrgaps,       {.i = +2 } },
-	{ MODKEY|Mod4Mask,              XK_k,      incrgaps,       {.i = -2 } },
+	{ MODKEY|Mod4Mask,              XK_j,      incrgaps,       {.i = +3 } },
+	{ MODKEY|Mod4Mask,              XK_k,      incrgaps,       {.i = -3 } },
 	{ MODKEY|Mod4Mask,              XK_0,	   defaultgaps,    {0} },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      togglegaps,     {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
