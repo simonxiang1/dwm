f44a90a703995127e099df0504e01b6494b59c10
Simon Xiang <simonxiang@zohomail.com>
2020-06-20 11:54:24 -0500

    swallow works (st --> St)

--- README.md
+++ README.md
@@ -13,8 +13,7 @@ todo list:
 	- ncmpcpp (pause, play, next song, shuffle?)
 - figure out how to get xinitrc to randomize wallpaper on startup
 - where did my extensive list of patches go reeeee
-- patches: alpha, centered master, cfacts(?), crop windows(?), ewmh (if switching to polybar/lemonbar), fibonacci (?), exresize for floating windows, tabs (for monocle mode), flextile(?? lots of reading), gapless grid + horizontal grid, movestack/push (?), mpdcontrol, next prev tag --> shiftview.c, rotate stack, save floats (floating aesthetic), sticky, stacker(?) and stackmfact (vertical resizing), three column layout (same as centered master?), xtile (??? lots of reading)
-
+- patches: centered master, cfacts(?), crop windows(?), ewmh (if switching to polybar/lemonbar), fibonacci (?), exresize for floating windows, tabs (for monocle mode), flextile(?? lots of reading), gapless grid + horizontal grid, movestack/push (?), mpdcontrol, next prev tag --> shiftview.c, rotate stack, save floats (floating aesthetic), sticky, stacker(?) and stackmfact (vertical resizing), three column layout (same as centered master?), xtile (??? lots of reading)
 
 done:
 - now uses mononoki nerd font
@@ -24,7 +23,7 @@ done:
   - alt-super-k to enlarge gaps
   - alt-super-0 to reset gaps and alt-super-shift-0 to toggle gaps
   - default gaps are a nice aesthetic 25
-- patched in swallow, pertag
+- patched in swallow, pertag (for swallow to work, change "st" to "St")
 - the bar now displays shulk's monado arts (with Source Han Sans to match the aesthetic)
 - the bar is now padded (dae bar is literally eris?? -probably aqua)
 - everything is blurred  (thanks compton??)
--- config.h
+++ config.h
@@ -3,16 +3,16 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int showbar            = 0;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const int vertpad            = 25;       /* vertical padding of bar */
+static const int sidepad            = 25;       /* horizontal padding of bar */
 static const char *fonts[]          = { 
 	"mononoki Nerd Font:size=11",
 	"Source Han Sans:size=11"
@@ -31,6 +31,7 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"compton", "--config", "/home/simon/.config/compton/compton.conf", NULL,
+	"wal", "-R", NULL,
 	"st", NULL,
 	NULL /* terminate */
 };
@@ -46,9 +47,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
-};
+	{ "St",      NULL,     NULL,           0,         0,          1,          -1,        -1 }, { NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */ };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
