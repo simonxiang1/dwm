d939a2ed54f35a2fa27cef38623fd30422c57ecd
Simon Xiang <simonxiang@zohomail.com>
2020-10-02 14:59:54 -0500

    scrot now saves to a directory

--- config.def.h
+++ config.def.h
@@ -130,7 +130,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_period, spawn,	   SHCMD("mpc next") },
 	{ MODKEY|ShiftMask,		XK_comma,  spawn,	   SHCMD("mpc seek -3") }, //seek fwd/bkd
 	{ MODKEY|ShiftMask,		XK_period, spawn,	   SHCMD("mpc seek +3") },
-	{ 0,				XK_Print,  spawn,	   SHCMD("scrot") }, //instascrot
+	{ 0,				XK_Print,  spawn,	   SHCMD("scrot -e 'mv $f ~/Desktop/personal/photos/screenshots'") }, //instascrot
 //	{ 0,				XK_F3,	   spawn,	   SHCMD("amixer -q sset Master 3%+") }, maybe ffmpeg/show key
 };
 
