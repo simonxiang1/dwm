f80cc79b8cc4f69faabce5dcf505e822206e06f6
Simon Xiang <simonxiang@zohomail.com>
2021-03-24 18:35:35 -0500

    patched in dwm-anybar, made the borders behave the way i want

--- config.h
+++ config.h
@@ -1,14 +1,17 @@
 /* testing git */
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar     = 1;          /* 1 means use non-dwm status bar */
+static const char *altbarclass = "Polybar";  /* Alternate bar class name */
+static const char *altbarcmd   = "/home/simon/.config/polybar/launch.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { 
 	"mononoki Nerd Font:size=11",
 	"Source Han Sans:size=11",
@@ -22,7 +25,7 @@ static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2},
 };
 
 static const char *const autostart[] = {
